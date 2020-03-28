01a366b7cf15db421fd18074eb208967751722f4
William Taylor <williammarktaylor@gmail.com>
2020-03-28 19:12:23 +1300

    Method to read string from Serial

--- ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox.ino
+++ ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox.ino
@@ -4,19 +4,64 @@
  Author:	William Taylor
 */
 
-// the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(9600);
     configureUnit();
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
   
 }
 
 void configureUnit() {
     printSplashScreen();
+    awaitUserInput();
+    clearScreen();
+    promptForNumberOf4DPoints();
+    promptForNumberOf4DPoints();
+}
+
+void awaitUserInput() {
+    while (!Serial.available()) {}
+    Serial.read();
+}
+
+char* getUserInput() {
+    byte rx_byte = 0;
+    char rx_string[13];
+    uint8_t i = 0;
+
+    while (true) {
+        if (Serial.available()) {
+            rx_byte = Serial.read();
+            Serial.write(rx_byte);
+            // If they have submitted their string, append NUL and return.
+            if (i < 13 && rx_byte == '\n' || rx_byte == '\r') {
+                rx_string[i] = '\0';
+                Serial.println();
+                return rx_string;
+            }
+            // Continue reading input till one less than the array size, leaving room for the NUL.
+            else if (i < 12 && rx_byte != '\n' && rx_byte != '\r') {
+                rx_string[i] = rx_byte;
+                i++;
+            }
+            else
+            {
+                Serial.println();
+                Serial.print("Entry too long. Enter input with less than 13 characters: ");
+                i = 0;
+                rx_byte = 0;
+            }
+        }
+    }
+}
+
+void clearScreen() {
+    Serial.write(27);       // ESC command
+    Serial.print("[2J");    // clear screen command
+    Serial.write(27);
+    Serial.print("[H");     // cursor to home command
 }
 
 void printSplashScreen() {
@@ -27,18 +72,10 @@ void printSplashScreen() {
     Serial.println("|                         |");
     Serial.println("+-------------------------+");
     Serial.println("To continue, press any key...");
-    awaitUserInput();
-    clearScreen();
 }
 
-void awaitUserInput() {
-    while (!Serial.available()) {
-    }
-}
-
-void clearScreen() {
-    Serial.write(27);       // ESC command
-    Serial.print("[2J");    // clear screen command
-    Serial.write(27);
-    Serial.print("[H");     // cursor to home command
+void promptForNumberOf4DPoints()
+{
+    Serial.print("How many 4D points do you wish to configure? (1-5): ");
+    getUserInput();
 }

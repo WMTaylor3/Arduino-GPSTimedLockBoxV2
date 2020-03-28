99b4506ed52232a16fb8100155c1865775ea5745
William Taylor <williammarktaylor@gmail.com>
2020-03-28 16:35:33 +1300

    Playing around with initial Serial UI

--- ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox.ino
+++ ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox.ino
@@ -6,10 +6,39 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+    Serial.begin(9600);
+    configureUnit();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
   
+}
+
+void configureUnit() {
+    printSplashScreen();
+}
+
+void printSplashScreen() {
+    Serial.println("+-------------------------+");
+    Serial.println("|                         |");
+    Serial.println("|    Timed GPS Lockbox    |");
+    Serial.println("|      Initial Setup      |");
+    Serial.println("|                         |");
+    Serial.println("+-------------------------+");
+    Serial.println("To continue, press any key...");
+    awaitUserInput();
+    clearScreen();
+}
+
+void awaitUserInput() {
+    while (!Serial.available()) {
+    }
+}
+
+void clearScreen() {
+    Serial.write(27);       // ESC command
+    Serial.print("[2J");    // clear screen command
+    Serial.write(27);
+    Serial.print("[H");     // cursor to home command
 }

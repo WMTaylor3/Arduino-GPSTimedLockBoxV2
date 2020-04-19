b02dfbbd432eca688bfdc6b4917c8b65928d7e1f
William Taylor <williammarktaylor@gmail.com>
2020-04-19 18:49:17 +1200

    Display class trimming.

--- ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/Display.cpp
+++ ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/Display.cpp
@@ -44,160 +44,126 @@ void Display::Write(String lineOne, String lineTwo = "")
 	}
 }
 
-void Display::WriteSearchBeginsIn(uint32_t days, uint32_t hours, uint32_t minutes)
+void Display::DaysHoursMinutes(uint8_t days, uint8_t hours, uint8_t minutes)
 {
-	Clear();
-	Write("Search will", "begin in...");
-	delay(3000);
 	if (days + hours + minutes <= 0)
 	{
 		Write("Less than", "a minute");
 	}
-	String dayString = String(days);
-	String hourString = String(hours);
-	String minuteString = String(minutes);
-	Write(dayString + " Days " + hourString + " Hours", minuteString + " Minutes");
+	else
+	{
+		Write(String(days) + " Days " + String(hours) + " Hours", String(minutes) + " Minutes");
+	}
+}
+
+void Display::DelayClear()
+{
 	delay(3000);
 	Clear();
+}
+
+void Display::WriteSearchBeginsIn(uint8_t days, uint8_t hours, uint8_t minutes)
+{
+	Write("Search will", "begin in...");
+	delay(3000);
+	DaysHoursMinutes(days, hours, minutes);
+	DelayClear();
 }
 
 void Display::WriteNextStageBeginsNow()
 {
-	Clear();
 	Write("Next stage", "commencing");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteStageXOfYComplete(uint8_t currentPoint, uint8_t totalPoints)
 {
-	Clear();
-	String currentPointString = String(currentPoint);
-	String totalPointsString = String(totalPoints);
-	Write("Stage #" + currentPointString + " of " + totalPointsString, "complete");
-	delay(3000);
-	Clear();
+	Write("Stage #" + String(currentPoint) + " of " + String(totalPoints), "complete");
+	DelayClear();
 }
 
 void Display::WriteObtainingGPSLocationFix()
 {
-	Clear();
 	Write("Obtaining GPS", "location fix...");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteDistanceRemaining(uint32_t distance)
 {
-	Clear();
 	Write("Distance to", "location...");
 	delay(3000);
-	String distanceString = String(distance);
-	Write(distanceString + " Meters");
-	delay(3000);
-	Clear();
+	Write(String(distance) + " Meters");
+	DelayClear();
 }
 
 void Display::WriteLocationReached()
 {
-	Clear();
 	Write("Location has", "been found");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
-void Display::WriteTimeToUnlock(uint32_t days, uint32_t hours, uint32_t minutes)
+void Display::WriteTimeToUnlock(uint8_t days, uint8_t hours, uint8_t minutes)
 {
-	Clear();
 	Write("Unlock window", "will start in...");
 	delay(3000);
-	if (days + hours + minutes <= 0)
-	{
-		Write("Less than", "a minute");
-	}
-	String dayString = String(days);
-	String hourString = String(hours);
-	String minuteString = String(minutes);
-	Write(dayString + " Days " + hourString + " Hours", minuteString + " Minutes");
-	delay(3000);
-	Clear();
+	DaysHoursMinutes(days, hours, minutes);
+	DelayClear();
 }
 
-void Display::WriteUnlockTimeRemaining(uint32_t days, uint32_t hours, uint32_t minutes)
+void Display::WriteUnlockTimeRemaining(uint8_t days, uint8_t hours, uint8_t minutes)
 {
-	Clear();
 	Write("Unlock window", "will last for...");
 	delay(3000);
-	if (days + hours + minutes <= 0)
-	{
-		Write("Less than", "a minute");
-	}
-	String dayString = String(days);
-	String hourString = String(hours);
-	String minuteString = String(minutes);
-	Write(dayString + " Days " + hourString + " Hours", minuteString + " Minutes");
-	delay(3000);
-	Clear();
+	DaysHoursMinutes(days, hours, minutes);
+	DelayClear();
 }
 
 void Display::WriteSerialMode()
 {
-	Clear();
 	Write("Serial mode");
 }
 
 void Display::WriteCalibratingRTC()
 {
-	Clear();
 	Write("Calibrating RTC", "from GPS fix...");
 }
 
 void Display::WriteRTCOffBy(uint32_t delta)
 {
-	Clear();
 	Write("Real Time Clock", "was off by...");
 	delay(3000);
 	Write(String(delta), "Seconds");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteTimeExtensionValues(uint8_t hours, uint8_t mins)
 {
-	Clear();
 	Write("Enter Value:");
 	lcd->setCursor(0, 1);
 	if (hours < 10) {
 		lcd->print('0');
 		lcd->setCursor(1, 1);
 	}
-	String hoursString = String(hours);
-	lcd->print(hoursString);
+	lcd->print(hours);
 	lcd->setCursor(14, 1);
 	if (mins < 10) {
 		lcd->print('0');
 		lcd->setCursor(15, 1);
 	}
-	String minutesString = String(mins);
 	lcd->print(mins);
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteTimeExtended()
 {
-	Clear();
 	Write("Time Extended", "Goodbye!");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteEnterPasscode()
 {
-	Clear();
 	Write("Passcode:");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::CharTyped(uint8_t dotCount)
@@ -209,36 +175,31 @@ void Display::CharTyped(uint8_t dotCount)
 void Display::WriteInsertBothKeys()
 {
 	Write("Insert second", "key to unlock");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteAccessGranted()
 {
 	Write("Access Granted", "Welcome");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteAccessDenied()
 {
 	Write("Access Denied");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteTooLate()
 {
 	Write("Too Late", "Window Missed");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::WriteGoodbye()
 {
 	Write("Goodbye");
-	delay(3000);
-	Clear();
+	DelayClear();
 }
 
 void Display::Clear()
--- ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/Display.h
+++ ArduinoGPSTimedLockBox/ArduinoGPSTimedLockBox/Display.h
@@ -21,19 +21,21 @@ class Display
 private:
 	static LiquidCrystal_I2C* lcd;
 	static void Write(String, String = "");
+	static void DaysHoursMinutes(uint8_t days, uint8_t hours, uint8_t minutes);
+	static void DelayClear();
 public:
 	Display();
 	static void Initialize();
 	static void LcdOn();
 	static void LcdOff();
-	static void WriteSearchBeginsIn(uint32_t, uint32_t, uint32_t);
+	static void WriteSearchBeginsIn(uint8_t, uint8_t, uint8_t);
 	static void WriteNextStageBeginsNow();
 	static void WriteStageXOfYComplete(uint8_t currentPoint, uint8_t totalPoints);
 	static void WriteObtainingGPSLocationFix();
 	static void WriteDistanceRemaining(uint32_t);
-	static void WriteTimeToUnlock(uint32_t, uint32_t, uint32_t);
+	static void WriteTimeToUnlock(uint8_t, uint8_t, uint8_t);
 	static void WriteLocationReached();
-	static void WriteUnlockTimeRemaining(uint32_t, uint32_t, uint32_t);
+	static void WriteUnlockTimeRemaining(uint8_t, uint8_t, uint8_t);
 	static void WriteSerialMode();
 	static void WriteCalibratingRTC();
 	static void WriteRTCOffBy(uint32_t delta);
